49eaa7c33e8e95191cd29edbcf362e5d54fed90e
Mahyar Shabani <mahyar.shaabani@yahoo.com>
2023-01-07 22:55:09 +0100

    refactor: move main.ts to app folder and write a task to transfer ts to js

---
+++ app/main.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require('electron');
+function createWindow() {
+    const win = new BrowserWindow({ width: 800, height: 800 });
+    win.loadFile('dist/nm-cleaner/index.html');
+    win.setMenu(null);
+}
+app.whenReady().then(() => {
+    createWindow();
+});
+//# sourceMappingURL=main.js.map
