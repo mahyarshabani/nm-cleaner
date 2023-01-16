a16ac754c9b5b01752e2ef6e99c29ec274d2ff90
Mahyar Shabani <mahyar.shaabani@yahoo.com>
2023-01-16 21:50:11 +0100

    style: fix indentation of app.component and quotations based on prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,24 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { from, Observable } from "rxjs";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { from, Observable } from 'rxjs';
 
 @Component({
-        selector: "app-root",
-        templateUrl: "./app.component.html",
-        styleUrls: ["./app.component.scss"],
-        changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-        title = "nm-cleaner";
-        foldersPath: Observable<string>;
+  title = 'nm-cleaner';
+  foldersPath: Observable<string>;
 
-        async openSelectFolderDialog() {
-                this.foldersPath = from(
-                        window.electronAPI.openSelectFolderDialog()
-                );
-                // window.electronAPI.openSelectFolderDialog();
-        }
+  async openSelectFolderDialog() {
+    this.foldersPath = from(window.electronAPI.openSelectFolderDialog());
+    // window.electronAPI.openSelectFolderDialog();
+  }
 
-        async scan() {
-                console.log("window.electronAPI");
-                console.log(window.electronAPI);
-                window.electronAPI.scan("C:\\Projects");
-                // console.log({ result });
-        }
+  async scan() {
+    console.log('window.electronAPI');
+    console.log(window.electronAPI);
+    window.electronAPI.scan('C:\\Projects');
+  }
 }
