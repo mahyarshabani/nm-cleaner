96e4906651561aff96f61072a4deb828108ac868
Mahyar Shabani <mahyar.shaabani@yahoo.com>
2023-01-28 22:50:28 +0100

    test: add basic tests for scan and app

--- src/app/pages/scan/scan.component.spec.ts
+++ src/app/pages/scan/scan.component.spec.ts
@@ -1,21 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScanComponent } from './scan.component';
+import { ElectronScanService, ElectronSelectService } from '../../services';
 
 describe('ScanComponent', () => {
   let component: ScanComponent;
   let fixture: ComponentFixture<ScanComponent>;
+  let electronScanService: ElectronScanService;
+  let electronSelectService: ElectronSelectService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ScanComponent ]
-    })
-    .compileComponents();
+  beforeEach(async(() => {
+    const electronScanService = jasmine.createSpyObj('ElectronScanService', [
+      'startScan',
+      'returnScanResult',
+    ]);
+    electronScanService.returnScanResult.and.returnValue([]);
+    const electronSelectService = jasmine.createSpyObj(
+      'ElectronSelectService',
+      ['openSelectFolder']
+    );
+    TestBed.configureTestingModule({
+      declarations: [ScanComponent],
+      providers: [
+        { provide: ElectronScanService, useValue: electronScanService },
+        { provide: ElectronSelectService, useValue: electronSelectService },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ScanComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
