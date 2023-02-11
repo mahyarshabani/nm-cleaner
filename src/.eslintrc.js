07337f565c8dd23ad54a7a890d8d1abded70155b
Mahyar Shabani <mahyar.shaabani@yahoo.com>
2023-02-11 20:12:53 +0100

    ci: add @angular-eslint/template-parser rules

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,12 +1,27 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['*.html'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      parser: "@angular-eslint/template-parser",
+      plugins: ["@angular-eslint/eslint-plugin-template"],
+      rules: {
+        "@angular-eslint/template/no-inline-styles": [
+          "error",
+          {
+            allowNgStyle: true,
+          },
+        ],
+      },
+      files: "**/*.html",
+      excludedFiles: "index.html",
+    },
+  ],
   root: true,
-  'env': {
-    'browser': true,
-    'amd': true,
-    'node': true
-  }
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
 };
