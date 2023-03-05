df16bba90bdf0870fd10554cea4416ec5ec9bda6
Mahyar Shabani <mahyar.shaabani@yahoo.com>
2023-03-05 23:59:15 +0100

    fix: fix windows path in  afterAllArtifactBuild.js based on new release path

--- scripts/afterAllArtifactBuild.js
+++ scripts/afterAllArtifactBuild.js
@@ -4,20 +4,20 @@ const zlib = require("zlib");
 const path = require("path");
 
 exports.default = async function (context) {
-  const windows = process.platform === 'win32';
+  const windows = process.platform === "win32";
   if (!windows) {
     return;
   }
   const exeFilePath = path.join(
     __dirname,
-    `../release/${packageJson.name}-portable-${packageJson.version}.exe`
+    `../release/${packageJson.name}-${packageJson.version}.portable.windows.exe`
   );
   try {
     await createZipFile(exeFilePath);
     console.log(`zip of the portable file is created in `);
     console.log(path.join(__dirname, `../release`));
   } catch (err) {
-    console.error(err);
+    console.error("zipping file failed: \n", err);
   }
 };
 
